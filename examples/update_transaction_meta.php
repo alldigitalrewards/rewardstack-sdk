95a0755b2c73ddc4c43da667a8ee6c1a7e7063ed
Mario Dujmovic <mario@alldigitalrewards.com>
2021-05-11 23:09:34 -0500

    DS-2638 update transaction meta added

---
+++ examples/update_transaction_meta.php
@@ -0,0 +1,32 @@
+<?php
+
+$client = require_once __DIR__ . '/bootstrap.php';
+
+use AllDigitalRewards\RewardStack\Transaction\AddTransactionMetaRequest;
+use AllDigitalRewards\RewardStack\Transaction\AddTransactionMetaResponse;
+
+$program = 'alldigitalrewards';
+$participantUniqueId = 'ADRTESTP1';
+$meta = [
+    [
+        "GAME_PLAY" => "WIN",
+    ],
+    [
+        "CAMPAIGN" => "Campaign",
+    ],
+    [
+        "GAME_PLAY_ENTRY" => 1,
+    ]
+];
+$addTransactionMetaRequest = new AddTransactionMetaRequest(
+    $program,
+    $participantUniqueId,
+    15,
+    $meta
+);
+
+/** @var AddTransactionMetaResponse $participantUpdateRequest */
+$addTransactionMetaResponse = $client->request($addTransactionMetaRequest);
+
+print_r($addTransactionMetaResponse);
+exit;
