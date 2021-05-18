a15da4549732ac3cd30f0e33524ed2bb8f42ed87
Mario Dujmovic <mario@alldigitalrewards.com>
2021-05-18 15:48:34 -0500

    DS-2683 - callout - remove unnecessary ?? []

--- src/Transaction/AddTransactionMetaRequest.php
+++ src/Transaction/AddTransactionMetaRequest.php
@@ -53,7 +53,7 @@ class AddTransactionMetaRequest extends AbstractApiRequest
         }
 
         return [
-            "meta" => $this->meta ?? [],
+            "meta" => $this->meta
         ];
     }
 }
