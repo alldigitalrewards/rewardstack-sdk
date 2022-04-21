9c942cff4b16c05a134800bd472d684eb4f1b9ac
jmuto <jmuto@alldigitalrewards.com>
2022-04-21 16:00:01 -0400

    DS-4052 add collection filter to allow for orderby on adjustments

--- examples/get_participant_adjustments.php
+++ examples/get_participant_adjustments.php
@@ -2,12 +2,19 @@
 
 $client = require_once __DIR__ . '/bootstrap.php';
 
+use AllDigitalRewards\RewardStack\Adjustment\AdjustmentCollectionFilter;
 use AllDigitalRewards\RewardStack\Adjustment\AdjustmentListRequest;
 use AllDigitalRewards\RewardStack\Adjustment\AdjustmentListResponse;
+use AllDigitalRewards\RewardStack\Common\Entity\OrderBy;
 
-$program = 'alldigitalrewards';
-$participantUniqueId = 'ADRTESTP1';
-$adjustmentRequest = new AdjustmentListRequest($program, $participantUniqueId);
+$program = 'aprogram';
+$participantUniqueId = 'aparticipant';
+$orderBy = new OrderBy();
+$orderBy->setField('adjustment.created_at');
+$orderBy->setDirection('desc');
+$filter = new AdjustmentCollectionFilter();
+$filter->setOrderBy($orderBy);
+$adjustmentRequest = new AdjustmentListRequest($program, $participantUniqueId, 1, 30, $filter);
 
 /** @var AdjustmentListResponse $adjustmentResponse */
 $adjustmentResponse = $client->request($adjustmentRequest);
---
+++ src/Adjustment/AdjustmentCollectionFilter.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace AllDigitalRewards\RewardStack\Adjustment;
+
+use AllDigitalRewards\RewardStack\Common\AbstractCollectionFilter;
+
+class AdjustmentCollectionFilter extends AbstractCollectionFilter
+{
+    public function getFilterArray(): array
+    {
+        return [];
+    }
+}
