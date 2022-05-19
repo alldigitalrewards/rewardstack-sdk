240bc0d0522923dcece6acbba44b078da09c7177
Zechariah Walden <zech@zewadesign.com>
2022-05-19 16:31:45 -0500

    DS-4163 - update test to utilize new fields

--- tests/Request/CreateAdjustmentRequestTest.php
+++ tests/Request/CreateAdjustmentRequestTest.php
@@ -13,6 +13,9 @@ class CreateAdjustmentRequestTest extends TestCase
     protected $type;
     protected $amount;
     protected $referenceId;
+    protected $description;
+    protected $activity;
+    protected $completedAt;
     protected $createAdjustmentRequest;
 
     protected function setup(): void
@@ -21,13 +24,19 @@ class CreateAdjustmentRequestTest extends TestCase
         $this->type = 'credit';
         $this->amount = 200;
         $this->referenceId = 'test-reference';
+        $this->description = 'a cool description';
+        $this->activity = 'cool-activitiy';
+        $this->completedAt = (new \DateTime())->format('Y-m-d H:i:s');
 
         $this->createAdjustmentRequest = new Adjustment\CreateAdjustmentRequest(
             $this->program,
             $this->uniqueId,
             $this->type,
             $this->amount,
-            $this->referenceId
+            $this->referenceId,
+            $this->description,
+            $this->activity,
+            $this->completedAt,
         );
     }
 
@@ -54,7 +63,9 @@ class CreateAdjustmentRequestTest extends TestCase
             "type" => $this->type,
             "amount" => $this->amount,
             "reference" => $this->referenceId,
-            "description" => null,
+            "description" => $this->description,
+            "activity" => $this->activity,
+            "completed_at" => $this->completedAt,
         ];
 
         $this->assertEquals(
